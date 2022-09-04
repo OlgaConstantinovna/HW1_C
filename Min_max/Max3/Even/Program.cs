a921d24eace3a499ce431d566167d9c0979bbf3c
Olga <princessca.olga@icloud.com>
2022-09-04 10:11:35 +0300

    new

---
+++ Min_max/Max3/Even/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = -7;
+
+if ((number % 2) == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
---
+++ Min_max/Program.cs
@@ -0,0 +1,10 @@
+﻿int a=2;
+int b=10;
+int min=a;
+int max=b;
+if (a>max) max = a;
+if (b<min) min = b;
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
